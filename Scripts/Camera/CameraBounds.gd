8a30949a130d2426162666ab8efde8163ff4110f
Ben Madsen <benjamin.matheron@hotmail.fr>
2024-02-24 16:21:37 +0100

    Camera Move only inside bounds

--- Scripts/Camera/CameraBounds.gd
+++ Scripts/Camera/CameraBounds.gd
@@ -2,15 +2,15 @@
 extends ReferenceRect
 
 
-var _camera_limits : Array[float] = [0,0,0,0]
-var CameraLimits : Array[float] :
+var _camera_limits : Array[int] = [0,0,0,0]
+var CameraLimits : Array[int] :
 	get : return _camera_limits
 	set(value) : 
 		_camera_limits = value
 		value_changed.emit(_camera_limits)
 
 
-signal value_changed(_limits : Array[float])
+signal value_changed(_limits : Array[int])
 
 
 
@@ -21,9 +21,9 @@ func _process(_delta):
 	_UpdateBounds()
 
 func _UpdateBounds():
-	var array : Array[float] = [0,0,0,0]
-	array[0] = position.x #			Limit Left
-	array[1] = position.y #				Limit Top
-	array[2] = position.x + size.x #	Limit Right
-	array[3] = position.y + size.y #	Limit Bottom
+	var array : Array[int] = [0,0,0,0]
+	array[0] = int(position.x) #			Limit Left
+	array[1] = int(position.y) #				Limit Top
+	array[2] = int(position.x + size.x) #	Limit Right
+	array[3] = int(position.y + size.y) #	Limit Bottom
 	if CameraLimits != array : CameraLimits = array
--- Scripts/Camera/CameraController.gd
+++ Scripts/Camera/CameraController.gd
@@ -1,27 +1,42 @@
 @tool
 extends Camera2D
+class_name CameraController
+
+@export var can_move : bool
+@export var move_speed : float = 3
+@export var zoom_speed : float
 
 @onready var cameraBounds : ReferenceRect =  $"../CameraBounds"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var touch_points : Dictionary = {} #To track multiple fingers input, used as Dynamic Array
 
 
 
+func _input(event):
+	if event is InputEventScreenTouch :
+		HandleTouch(event)
+	elif event is InputEventScreenDrag :
+		HandleDrag(event)
 	
-
+	
+func HandleTouch(event : InputEventScreenTouch):
+	if event.pressed :
+		touch_points[event.index] = event.position
+	else : touch_points.erase(event.index)
+		
+	
+func HandleDrag(event : InputEventScreenDrag):
+	touch_points[event.index] = event.position
+	
+	if touch_points.size() == 1:
+		if can_move :
+			position -= event.relative * move_speed
 
 func _on_camera_bounds_value_changed(_limits):
 	SetLimits(_limits)
-	print(_limits)
 
-func SetLimits(_limits : Array[float]):
+
+func SetLimits(_limits : Array[int]):
 	limit_left = _limits[0]
 	limit_top = _limits[1]
 	limit_right = _limits[2]
