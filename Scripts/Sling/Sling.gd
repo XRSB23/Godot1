ffc2bb7cb059608242e6b3f79d30e23fe817db0c
Ben Madsen <benjamin.matheron@hotmail.fr>
2024-02-26 21:53:19 +0100

    Converted Sling input to Touch Input

--- Scripts/Sling/Sling.gd
+++ Scripts/Sling/Sling.gd
@@ -6,39 +6,62 @@ var bubble_prefab = preload("res://scenes/bubble.tscn")
 @onready var trajectory_preview : TrajectoryPreview = $TrajectoryPreview 
 @onready var debug_drag_vector_gizmo = $Debug_DragVector_Gizmo
 
-
+var touch_points : Dictionary = {} #To track multiple fingers input, used as Dynamic Array
+var start_point = Vector2.ZERO
+var input_direction = Vector2.ZERO
+var scaled_v
 var balls_amount : int
 var ball
+var valid_shot : bool
 @export var trajectory_mode : TrajectoryPreview.MODE = TrajectoryPreview.MODE.VECTOR
 @export var shoot_strength : float
 @export var min_drag : float
 @export var max_drag : float
 @export var drag_curve : Curve
 
+
 func _ready():
 	if debug_drag_vector_gizmo.visible && !OS.has_feature("editor"):
 		debug_drag_vector_gizmo.visible = false
 
-
 func _input(event):
-	if ball != null and ball.is_dragging :
+	if ball && ball.is_dragging :
+		if event is InputEventScreenTouch :
+			HandleTouch(event)
+		elif event is InputEventScreenDrag :
+			HandleDrag(event)
+	
+
+
+func HandleTouch(event):
+
+	if event.pressed :
+		touch_points[event.index] = event.position
+	else:
+		start_point = Vector2.ZERO
+		debug_drag_vector_gizmo.points = [Vector2.ZERO,Vector2.ZERO]
+		if touch_points.size() < 2 :
+			if valid_shot : shoot_ball(scaled_v)
+			else : cancel_shot()
+
 		
-		var v : Vector2 = position - get_global_mouse_position()
-		v = v.normalized() * min(max_drag, v.length()) # Cap Drag lenghth
-		debug_drag_vector_gizmo.points[1] = -v
-		var drag_ratio = v.length()/max_drag
-		var scaled_v = v.normalized() * drag_curve.sample(drag_ratio) * max_drag
+
+func HandleDrag(event):
+	touch_points[event.index] = event.position
+	if start_point == Vector2.ZERO : start_point = get_local_mouse_position()
+	if touch_points.size() == 1:
+		debug_drag_vector_gizmo.points[0] = start_point
+		input_direction = start_point - get_local_mouse_position()
+		input_direction = input_direction.normalized() * min(max_drag, input_direction.length()) # Cap Drag lenghth
+		debug_drag_vector_gizmo.points[1] = -input_direction
+		var drag_ratio = input_direction.length()/max_drag
+		scaled_v = input_direction.normalized() * drag_curve.sample(drag_ratio) * max_drag
 		
+		valid_shot = input_direction.x > 0 && input_direction.length() > min_drag #Replace If(Condition) true else false
 		
-		if v.x > 0 :
-			if event is InputEventMouseButton and event.pressed == false :
-				if v.length() > min_drag :
-					shoot_ball(scaled_v)
-				else :
-					cancel_shot()
-			else: trajectory_preview.Display(trajectory_mode, scaled_v, shoot_strength)
-		else : 
-			cancel_shot()
+		if valid_shot : trajectory_preview.Display(trajectory_mode, scaled_v, shoot_strength)
+		else : trajectory_preview.ClearPreview()
+
 
 func cancel_shot() :
 	ball.is_dragging = false
