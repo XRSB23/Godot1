0149a9b156e3f8741631a76c05aee232b5cf82e9
Ben Madsen <benjamin.matheron@hotmail.fr>
2024-02-27 21:04:53 +0100

    Vector Shader

--- Scripts/Sling/VectorTrajectoryPreview.gd
+++ Scripts/Sling/VectorTrajectoryPreview.gd
@@ -1,9 +1,11 @@
 extends Line2D
 @onready var sling = $"../.."
+const COLOR_ATLAS_RESOURCE = preload("res://Resources/ColorAtlas_Resource.tres")
 
 @export var min_width : int = 50
 @export var max_width : int = 100
 @export var origin_offset : float
+var length_ratio : float
 
 
 func _ready():
@@ -11,6 +13,19 @@ func _ready():
 
 
 func display_trajectory(v : Vector2):
-	width = min_width + (max_width - min_width) * v.length() / sling.max_drag
+	SetColor()
+	length_ratio = v.length() / sling.max_drag
+	width = min_width + (max_width - min_width) * length_ratio
+	material.set_shader_parameter("LengthRatio", length_ratio)
 	add_point(Vector2.ZERO + v.normalized() * origin_offset)
 	add_point(v)
+
+func SetColor():
+	if sling.ball :
+		var color = sling.ball.color
+		if color < COLOR_ATLAS_RESOURCE.colors.size() :
+			material.set_shader_parameter ("InputColor", COLOR_ATLAS_RESOURCE.GetColor(color))
+		else :
+			material.set_shader_parameter ("InputColor", COLOR_ATLAS_RESOURCE.GetColor(0))
+			push_warning("Sling Ball Color unrecognized by ColorAtlas, set to ColorAtlas[0] (White) by default")
+				
