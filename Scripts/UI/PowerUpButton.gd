0c0f28f419657725a60f98efc0393d5ac8bddb30
Zedashtar <benjamin.matheron@gmail.com>
2024-04-29 17:12:09 +0200

    PowerUpMenu Basic Behavior
    Highlight & Disable behavior w/ shader for both ShootMode & Projectile

--- Scripts/UI/PowerUpButton.gd
+++ Scripts/UI/PowerUpButton.gd
@@ -1,11 +1,54 @@
 extends TextureButton
+class_name  PowerUpButton
 
+@export_category("Node Connexion")
+@export var camera : CameraController
+@export var consumable_panel : PowerUpPanel
+@export var amount_label : Label
+@export var highlight : TextureRect
+@export var icon : TextureRect
 
-# Called when the node enters the scene tree for the first time.
+@export_category("PowerUp")
+enum TYPE {ShootMode, Projectile}
+@export var type : TYPE
+
+@export_category("Button")
+@export var is_highlighted : bool = false
+
 func _ready():
-	pass # Replace with function body.
+	Update()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func Update():
+	if consumable_panel.infinite_powerups && OS.has_feature("editor"):
+		amount_label.text = "∞"
+		Disable(false)
+	else :
+		amount_label.text = str(SaveData.inventory[name])
+		Disable(SaveData.inventory[name] <= 0)
+
+	
+func Disable(b: bool):
+	if !b && SaveData.inventory[name] <= 0 && !consumable_panel.infinite_powerups:
+		return
+	
+	disabled = b
+	icon.material.set_shader_parameter("is_grayscale", b)
+	
+	
+
+func Highlight(b :bool):
+	highlight.modulate = Color(1,1,1,1) if b else Color(1,1,1,0)
+	
+	
+func _on_button_down():
+	camera.EnableControls(false)
+	match type :
+		TYPE.ShootMode :
+			consumable_panel.SelectMode(null if consumable_panel.selected_mode == self else self)
+		TYPE.Projectile :
+			consumable_panel.SelectProjectile(null if consumable_panel.selected_projectile == self else self)
+
+
+func _on_button_up():
+	camera.EnableControls(true)
