e1a8931a4dc1b05d26396428516bf93350526292
Zedashtar <benjamin.matheron@gmail.com>
2024-05-15 18:19:14 +0200

    Fixing UI Problems

--- Scripts/Sling/PickupRadius.gd
+++ Scripts/Sling/PickupRadius.gd
@@ -4,19 +4,12 @@ extends Area2D
 @onready var sling = $".."
 
 
-func _input(event):
-	if event is InputEventScreenTouch && !event.pressed && sling.ball != null:
-		if sling.ball.is_dragging:
-			gamescene.camera.EnableControls(true)
-
-
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventScreenTouch && sling.ball != null:
 		if event.pressed :
 			sling.ball.is_dragging = true 
-			gamescene.camera.EnableControls(false)
 		else :
 			sling.ball.is_dragging = false 
-			gamescene.camera.EnableControls(true)
+
 
